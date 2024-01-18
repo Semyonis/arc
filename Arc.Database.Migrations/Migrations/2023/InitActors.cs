167b35a69291d78bc8c039efba6a94aa6483af12
simeoniys <simeoniys@gmail.com>
2024-01-19 02:07:41 +0600

    migration stage in dockerfile

--- Arc.Database.Migrations/Migrations/2023/InitActors.cs
+++ Arc.Database.Migrations/Migrations/2023/InitActors.cs
@@ -16,16 +16,20 @@ public partial class InitActors : Migration
             .Sql(
                 """
                 insert into asp_net_role (id, name, normalized_name)
-                values ('admin','Admin','admin'), ('user','User','user');
+                values ('admin','Admin','admin'),
+                       ('user','User','user');
 
                 insert into asp_net_user(id, user_name, normalized_user_name, email, normalized_email, password_hash, security_stamp, concurrency_stamp,email_confirmed,phone_number_confirmed, two_factor_enabled, lockout_end, lockout_enabled, access_failed_count)
-                values ('first_user','user@domain.com','user@domain.com','user@domain.com','user@domain.com','AQAAAAEAACcQAAAAEGyQG3jnaW0zbFnb2t9iJXkF+8fKcxMD8rs7846WsAQ3TKwffnCNzsrDD/Fvx52+Ow==','RUOAHR4WTP6GXUVNZI5LW6AZGZU4BM22','f740e66d-b985-44dc-9640-8a497172198e',1,0,0,0,0,0), ('first_admin','admin@domain.com','admin@domain.com','admin@domain.com','admin@domain.com','AQAAAAEAACcQAAAAEGyQG3jnaW0zbFnb2t9iJXkF+8fKcxMD8rs7846WsAQ3TKwffnCNzsrDD/Fvx52+Ow==','RUOAHR4WTP6GXUVNZI5LW6AZGZU4BM22','f740e66d-b985-44dc-9640-8a497172198e',1,0,0,0,0,0);
+                values ('first_user','user@domain.com','user@domain.com','user@domain.com','user@domain.com','AQAAAAEAACcQAAAAEGyQG3jnaW0zbFnb2t9iJXkF+8fKcxMD8rs7846WsAQ3TKwffnCNzsrDD/Fvx52+Ow==','RUOAHR4WTP6GXUVNZI5LW6AZGZU4BM22','f740e66d-b985-44dc-9640-8a497172198e',1,0,0,'1970-01-01 00:00:00.000000',0,0),
+                       ('first_admin','admin@domain.com','admin@domain.com','admin@domain.com','admin@domain.com','AQAAAAEAACcQAAAAEGyQG3jnaW0zbFnb2t9iJXkF+8fKcxMD8rs7846WsAQ3TKwffnCNzsrDD/Fvx52+Ow==','RUOAHR4WTP6GXUVNZI5LW6AZGZU4BM22','f740e66d-b985-44dc-9640-8a497172198e',1,0,0,'1970-01-01 00:00:00.000000',0,0);
 
                 insert into asp_net_user_role(user_id, role_id)
-                values ('first_user','user'), ('first_admin','admin');
+                values ('first_user','user'),
+                       ('first_admin','admin');
 
                 insert into actor(id, discriminator, email, first_name, last_name)
-                values (1,'Admin', 'admin@domain.com','First','Last'),(2,'User', 'user@domain.com','First','Last');
+                values (1,'Admin', 'admin@domain.com','First','Last'),
+                       (2,'User', 'user@domain.com','First','Last');
                 """
             );
     }
--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,34 @@
+################################################################################
+# BUILD
+################################################################################
 FROM mcr.microsoft.com/dotnet/sdk:8.0-alpine AS build
 WORKDIR /source
 COPY . .
 RUN dotnet restore 
 RUN dotnet build --no-restore
 
+
+################################################################################
+# TEST
+################################################################################
 FROM build AS test
 WORKDIR /source
 RUN dotnet test  --no-build --verbosity normal
 
+
+################################################################################
+# MIGRATION
+################################################################################
+FROM build AS migration
+WORKDIR /source/Arc.Executable.Migration/bin/Debug/net8.0
+COPY ./Arc.Executable.Migration/appsettings.json ./appsettings.json
+
+ENTRYPOINT ["dotnet", "Arc.Executable.Migration.dll"]
+
+
+################################################################################
+# DEVELOPMENT
+################################################################################
 FROM build AS development
 WORKDIR /source/Arc.Executable.WebApi/bin/Debug/net8.0
 COPY ./Arc.Executable.WebApi/appsettings.json ./appsettings.json
