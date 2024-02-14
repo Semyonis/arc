31344bce167f6896a09695bb952249e7b4a372ec
simeoniys <simeoniys@gmail.com>
2024-02-14 19:57:24 +0600

    Arc.Executable.WebApi.Silk proj init commit

---
+++ Arc.Executable.WebApi.Silk/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Sisk.Core.Http;
+using Sisk.Core.Routing;
+
+const string HttpLocalhost =
+    "http://localhost:5000/";
+
+var app =
+    HttpServer
+        .CreateBuilder(
+            host =>
+                host
+                    .UseListeningPort(
+                        HttpLocalhost
+                    )
+        );
+
+app
+    .Router
+    .SetRoute(
+        RouteMethod.Get,
+        "/",
+        _ =>
+            new HttpResponse(
+                    200
+                )
+                .WithContent(
+                    "Hello, world!"
+                )
+    );
+
+app.Start();
